3a4ace0a694c9e3ff20c3d38626b45d89273d31c
dylansarikas <dylan.sarikas@gmail.com>
2024-07-22 16:05:47 -0500

    runs migrations

--- app/models/tailgate.rb
+++ app/models/tailgate.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: tailgates
+#
+#  id         :integer          not null, primary key
+#  location   :string
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  stadium_id :integer          not null
+#
+# Indexes
+#
+#  index_tailgates_on_stadium_id  (stadium_id)
+#
+# Foreign Keys
+#
+#  stadium_id  (stadium_id => stadiums.id)
+#
 class Tailgate < ApplicationRecord
   belongs_to :stadium
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_22_201858) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_22_210348) do
   create_table "hotels", force: :cascade do |t|
     t.string "name"
     t.string "link"
@@ -30,6 +30,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_22_201858) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "tailgates", force: :cascade do |t|
+    t.string "location"
+    t.string "name"
+    t.integer "stadium_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["stadium_id"], name: "index_tailgates_on_stadium_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -44,4 +53,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_22_201858) do
   end
 
   add_foreign_key "hotels", "stadiums"
+  add_foreign_key "tailgates", "stadiums"
 end

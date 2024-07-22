1492f03ea413d25e53c48771eb3e5a3b31d02513
dylansarikas <dylan.sarikas@gmail.com>
2024-07-22 14:40:58 -0500

    adds seed file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  root "stadiums#index"
+
   resources :stadiums
   devise_for :users
-  root "stadium#show"
 
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
 

d893c92e94b74a22c05cabd742fd43e26a5852ef
dylansarikas <dylan.sarikas@gmail.com>
2024-07-29 16:01:35 -0500

    adds tailgates to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def index
+    @gb = Stadium.find_by(name: "Lambeau Field")
+    @chi = Stadium.find_by(name: "Soldier Field")
+    @det = Stadium.find_by(name: "Ford Field")
+    @min = Stadium.find_by(name: "US Bank Stadium")
   end
 end

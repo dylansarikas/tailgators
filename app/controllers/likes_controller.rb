67e098fbba3b88c6a9cba73979054460cf0683db
dylansarikas <dylan.sarikas@gmail.com>
2024-08-17 11:20:50 -0500

    cleans up application controller and likes controler

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -7,10 +7,10 @@ class LikesController < ApplicationController
 
   def create
     @like = Like.new(like_params)
-    
+
     respond_to do |format|
       if @like.save
-        format.html { redirect_back_or_to root_url, notice: "Like was successfully created." }
+        format.html { redirect_back_or_to root_url, notice: 'Like was successfully created.' }
         format.json { render :show, status: :created, location: @like }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -22,17 +22,18 @@ class LikesController < ApplicationController
   def destroy
     @like.destroy
     respond_to do |format|
-      format.html { redirect_back_or_to root_url, notice: "Like was successfully destroyed." }
+      format.html { redirect_back_or_to root_url, notice: 'Like was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    def set_like
-      @like = Like.find(params[:id])
-    end
 
-    def like_params
-      params.require(:like).permit(:fan_id, :advice_id)
-    end
+  def set_like
+    @like = Like.find(params[:id])
+  end
+
+  def like_params
+    params.require(:like).permit(:fan_id, :advice_id)
+  end
 end
